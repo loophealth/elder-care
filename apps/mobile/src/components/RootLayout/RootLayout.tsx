29c22d18f3414b78bbaddf73a3df1f9e64b932a2
Ankur Sethi <contact@ankursethi.in>
2023-01-23 18:41:21 +0530

    Style background

--- apps/mobile/src/components/RootLayout/RootLayout.tsx
+++ apps/mobile/src/components/RootLayout/RootLayout.tsx
@@ -1,21 +1,11 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-import logo from "static/img/logo.svg";
+import "./RootLayout.css";
 
 export const RootLayout = () => {
   return (
-    <>
-      <header>
-        <Link to="/">
-          <img
-            src={logo}
-            alt="Loop Health logo, the word 'loop' written in loopy, curly handwriting using green ink"
-          />
-        </Link>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <div className="RootLayout">
+      <Outlet />
+    </div>
   );
 };
