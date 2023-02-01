19522ff6fec64eda647555b9f863f07a19e129d3
Ankur Sethi <contact@ankursethi.in>
2023-02-01 15:09:22 +0530

    Remove bottom card from the home route

--- apps/mobile/src/routes/HomeRoute/HomeRoute.tsx
+++ apps/mobile/src/routes/HomeRoute/HomeRoute.tsx
@@ -1,95 +1,29 @@
-import { useState } from "react";
 import { useAuth } from "@loophealth/api";
 
-import { BottomCard } from "components/BottomCard";
-import { Button } from "components/Button";
-import { IconButton } from "components/IconButton";
-import { TimelineList } from "components/TimelineList";
 import { signOut } from "lib/firebaseHelpers";
 import { useSafeArea } from "lib/useSafeArea";
-import { useLocalStorage } from "lib/useLocalStorage";
 
 import "./HomeRoute.css";
 
 export const HomeRoute = () => {
   const { user } = useAuth();
-  const [isModalOpen, setIsModalOpen] = useState(true);
   const { isLoadingSafeAreaInsets, safeAreaInsets } = useSafeArea();
-
-  const [didShowFirstRunModal, setDidShowFirstRunModal] = useLocalStorage(
-    "didShowFirstRunModal",
-    false
-  );
 
   const onLogOut = async () => {
     await signOut();
   };
 
-  const onCloseFirstRunModal = () => {
-    setIsModalOpen(false);
-    setDidShowFirstRunModal(true);
-  };
-
-  if (isLoadingSafeAreaInsets) {
+  if (isLoadingSafeAreaInsets || !safeAreaInsets) {
     return null;
   }
 
   return (
     <div
       className="HomeRoute"
-      style={{ paddingTop: `${safeAreaInsets?.top}px` }}
+      style={{ paddingTop: `${safeAreaInsets.top}px` }}
     >
       <p>You are currently logged in as: {user?.phoneNumber ?? "unknown"}</p>
       <button onClick={onLogOut}>Log Out</button>
-      {didShowFirstRunModal ? null : (
-        <BottomCard
-          isOpen={isModalOpen}
-          renderContent={() => (
-            <BottomCardContent onClose={onCloseFirstRunModal} />
-          )}
-        />
-      )}
-    </div>
-  );
-};
-
-const BottomCardContent = ({ onClose }: { onClose: () => void }) => {
-  return (
-    <div className="HomeRoute__BottomCardContent">
-      <div className="HomeRoute__BottomCardContent__Header">
-        <IconButton
-          iconPath="/img/close.svg"
-          alt="Close modal dialog"
-          onClick={onClose}
-        />
-      </div>
-      <h1>Your past, present, and future health — all in one place</h1>
-      <p>
-        Loop automatically imports reports, consultations and prescriptions and
-        create an accurate forecast of your future health.
-      </p>
-
-      <p>
-        Now you can prevent serious conditions even before they present any
-        signs.
-      </p>
-
-      <TimelineList
-        items={[
-          { label: "Past reports", when: "past" },
-          { label: "Current checkup", when: "past" },
-          { label: "Current care plan", when: "present" },
-          { label: "Follow-up scheduled", when: "future" },
-          { label: "Future health forecast", when: "future" },
-        ]}
-      />
-
-      <Button
-        className="HomeRoute__BottomCardContent__Button"
-        onClick={onClose}
-      >
-        Go to Timeline
-      </Button>
     </div>
   );
 };
