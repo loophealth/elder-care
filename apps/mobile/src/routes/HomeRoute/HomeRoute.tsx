5422b74c38acfc14706402293c12abd629844177
Ankur Sethi <contact@ankursethi.in>
2023-02-13 16:55:18 +0530

    Add timeline ticks and timeline dots to the home view

--- apps/mobile/src/routes/HomeRoute/HomeRoute.tsx
+++ apps/mobile/src/routes/HomeRoute/HomeRoute.tsx
@@ -25,35 +25,44 @@ export const HomeRoute = () => {
   }
 
   return (
-    <div className="HomeRoute">
-      <div className="HomeRoute__FollowUps">
-        {futureFollowUps?.map((followUp) => (
-          <div key={followUp.title} className="HomeRoute__FollowUps__FollowUp">
-            <div className="Utils__Label HomeRoute__FollowUps__FollowUp__Date">
-              {format(followUp.date.toDate(), "do MMMM, yyyy")}
+    <main className="HomeRoute">
+      <div className="HomeRoute__Timeline">
+        <div className="HomeRoute__Timeline__TimelineTicks" />
+      </div>
+      <div className="HomeRoute__Content">
+        <div className="HomeRoute__FollowUps">
+          {futureFollowUps?.map((followUp) => (
+            <div
+              key={followUp.title}
+              className="HomeRoute__FollowUps__FollowUp"
+            >
+              <div className="HomeRoute__Dot HomeRoute__Dot--Inactive" />
+              <div className="Utils__Label HomeRoute__FollowUps__FollowUp__Date">
+                {format(followUp.date.toDate(), "do MMMM, yyyy")}
+              </div>
+              <div className="HomeRoute__FollowUps__FollowUp__Reason">
+                {followUp.title}
+              </div>
             </div>
-            <div className="HomeRoute__FollowUps__FollowUp__Reason">
-              {followUp.title}
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="HomeRoute__RiskFactors">
+          <div className="Utils__Label">Your future risk factors</div>
+          {patient?.profile.riskFactors.map((riskFactor) => (
+            <RiskFactorTile key={riskFactor.name} riskFactor={riskFactor} />
+          ))}
+        </div>
+        <div className="HomeRoute__Logout">
+          <Button
+            className="HomeRoute__Logout__Button"
+            variant={ButtonVariant.Danger}
+            onClick={onLogOut}
+          >
+            Log out of the app
+          </Button>
+          <p>You are logged in as: {user?.phoneNumber ?? "unknown"}</p>
+        </div>
       </div>
-      <div className="HomeRoute__RiskFactors">
-        <div className="Utils__Label">Your future risk factors</div>
-        {patient?.profile.riskFactors.map((riskFactor) => (
-          <RiskFactorTile key={riskFactor.name} riskFactor={riskFactor} />
-        ))}
-      </div>
-      <div className="HomeRoute__Logout">
-        <Button
-          className="HomeRoute__Logout__Button"
-          variant={ButtonVariant.Danger}
-          onClick={onLogOut}
-        >
-          Log out of the app
-        </Button>
-        <p>You are logged in as: {user?.phoneNumber ?? "unknown"}</p>
-      </div>
-    </div>
+    </main>
   );
 };
